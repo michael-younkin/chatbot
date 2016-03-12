5ee9840008adfee07e7698ace3a5f277036ebdc2
Michael Blais Younkin <younkin.michael@gmail.com>
2016-03-11 23:35:23 -0500

    Add a nicer prompt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io::BufRead;
+use std::io::Write;
 use std::io;
 use std::collections::HashSet;
 use rand::Rng;
@@ -9,9 +10,14 @@ fn main() {
     let mut seen_input: Vec<String> = Vec::new();
     let mut rng = rand::thread_rng();
     let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    print!("User: ");
+    stdout.flush().unwrap();
     for line in stdin.lock().lines().map(|l| l.unwrap()) {
         seen_input.push(line);
         let i = rng.gen_range(0, seen_input.len());
-        println!("{}", seen_input[i]);
+        println!(">> {}", seen_input[i]);
+        print!("User: ");
+        stdout.flush().unwrap();
     }
 }
