9bf29362cd443b4d246c0b1237035cb45ab94261
Michael Blais Younkin <younkin.michael@gmail.com>
2016-03-11 23:31:04 -0500

    Read lines instead of all stdin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate rand;
 
-use std::io::Read;
+use std::io::BufRead;
 use std::io;
 use std::collections::HashSet;
 use rand::Rng;
@@ -8,10 +8,9 @@ use rand::Rng;
 fn main() {
     let mut seen_input: Vec<String> = Vec::new();
     let mut rng = rand::thread_rng();
-    loop {
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer).unwrap();
-        seen_input.push(buffer);
+    let stdin = io::stdin();
+    for line in stdin.lock().lines().map(|l| l.unwrap()) {
+        seen_input.push(line);
         let i = rng.gen_range(0, seen_input.len());
         println!("{}", seen_input[i]);
     }
