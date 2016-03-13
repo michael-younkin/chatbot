888711ca75f767c28cb4f4482ed3105f1b13f380
Michael Blais Younkin <younkin.michael@gmail.com>
2016-03-13 11:56:51 -0400

    Add markhov chain based response generation

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,124 @@ use std::io::Write;
 use std::io;
 use std::collections::HashSet;
 use rand::Rng;
+use std::collections::HashMap;
+
+#[derive(Clone, Eq, Hash, PartialEq)]
+enum Token {
+    InputStart,
+    Word(String),
+    InputEnd
+}
+
+impl Token {
+    fn is_not_end(&self) -> bool {
+        match self {
+            &Token::InputEnd => false,
+            _ => true
+        }
+    }
+}
+
+struct TokenContext {
+    token: Token,
+    next_tokens: HashMap<Token, u32>,
+    total_weight: u32,
+}
+
+impl TokenContext {
+    fn new(token: &Token) -> TokenContext {
+        TokenContext {
+            token: token.to_owned(),
+            next_tokens: HashMap::new(),
+            total_weight: 0
+        }
+    }
+
+    fn total_weight(&self) -> u32 {
+        self.total_weight
+    }
+
+    fn add_next(&mut self, token: &Token) {
+        let count = self.next_tokens.entry(token.to_owned()).or_insert(0);
+        *count += 1;
+        self.total_weight += 1;
+    }
+
+    fn get_weighted(&self, mut index: u32) -> &Token {
+        if index >= self.total_weight {
+            panic!("Index out of bounds.");
+        }
+        for (k,v) in self.next_tokens.iter() {
+            if index < *v {
+                return k
+            } else {
+                index -= *v;
+            }
+        }
+        // This should never happen if we count total_weight properly.
+        panic!("Index out of bounds.");
+    }
+}
+
+struct KnowledgeBase {
+    tokens: HashMap<Token, TokenContext>
+}
+
+impl KnowledgeBase {
+    fn new() -> KnowledgeBase {
+        KnowledgeBase {
+            tokens: HashMap::new()
+        }
+    }
+
+    fn add_pair(&mut self, left: &Token, right: &Token) {
+        let left_context = self.tokens.entry(left.to_owned())
+            .or_insert_with(|| TokenContext::new(left));
+        left_context.add_next(right);
+    }
+
+    fn num_tokens(&self) -> usize {
+        self.tokens.len()
+    }
+
+    fn gen_output<Rand>(&self, rng: &mut Rand) -> String where Rand: Rng{
+        let mut out = String::new();
+        let mut last_token = &Token::InputStart;
+        while last_token.is_not_end() {
+            // Unwrap because the only time this will happen is if the database got corrupted or if
+            // we don't have anything inserted yet. TODO add better behavior here later (maybe
+            // default contents of the knowledgebase?).
+            let context = self.tokens.get(last_token).unwrap();
+            last_token = context.get_weighted(rng.gen_range(0, context.total_weight()));
+            if let &Token::Word(ref s) = last_token {
+                out.push_str(s);
+                out.push_str(" ");
+            }
+            println!("{}", &out);
+        }
+        out.pop();
+        out
+    }
+}
 
 fn main() {
-    let mut seen_input: Vec<String> = Vec::new();
     let mut rng = rand::thread_rng();
     let stdin = io::stdin();
     let mut stdout = io::stdout();
+    let mut kb = KnowledgeBase::new();
     print!("User: ");
     stdout.flush().unwrap();
     for line in stdin.lock().lines().map(|l| l.unwrap()) {
-        seen_input.extend(line.split_whitespace().map(|s| s.to_owned()));
-        print!(">> ");
-        if seen_input.len() > 0 {
-            let response_length = rng.gen_range(3, 15);
-            for i in 0..response_length {
-                print!("{} ", seen_input[rng.gen_range(0, seen_input.len())]);
-            }
-            println!("");
-        } else {
-            println!("...");
+        let token_pairs = line.split_whitespace().map(|word| Token::Word(word.to_owned()));
+        let mut left = Token::InputStart;
+        for right in token_pairs {
+            kb.add_pair(&left, &right);
+            left = right;
         }
+        kb.add_pair(&left, &Token::InputEnd);
+
+        println!(">> {}", kb.gen_output(&mut rng));
+
         print!("User: ");
         stdout.flush().unwrap();
     }
