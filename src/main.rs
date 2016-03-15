682e43ca6b7b51b21d00cbfc378c1345a06beb3e
Michael Blais Younkin <younkin.michael@gmail.com>
2016-03-14 20:04:24 -0400

    Add tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate rand;
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
 
 use std::io::BufRead;
 use std::io::Write;
@@ -6,20 +9,71 @@ use std::io;
 use std::collections::HashSet;
 use rand::Rng;
 use std::collections::HashMap;
+use regex::Regex;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+enum TokenKind {
+    InputStart,
+    Punct,
+    Word,
+    Space,
+    InputEnd,
+}
+
+impl TokenKind {
+    fn as_str(self) -> &'static str {
+        match self {
+            TokenKind::InputStart => "InputStart",
+            TokenKind::InputEnd => "InputEnd",
+            TokenKind::Punct => "Punct",
+            TokenKind::Word => "Word",
+            TokenKind::Space => "Space",
+        }
+    }
+
+    fn with_name(s: &str) -> TokenKind {
+        match s {
+            "InputStart" => TokenKind::InputStart,
+            "InputEnd" => TokenKind::InputEnd,
+            "Punct" => TokenKind::Punct,
+            "Word" => TokenKind::Word,
+            "Space" => TokenKind::Space,
+            _ => panic!("Invalid name"),
+        }
+    }
+}
 
 #[derive(Clone, Eq, Hash, PartialEq)]
-enum Token {
-    InputStart,
-    Word(String),
-    InputEnd
+struct Token {
+    value: Option<String>,
+    kind: TokenKind
 }
 
 impl Token {
+    fn new(kind: TokenKind) -> Token {
+        Token {
+            value: None,
+            kind: kind
+        }
+    }
+
+    fn with_value(kind: TokenKind, value: &str) -> Token {
+        // TODO consider panicing if kind shouldn't have a value
+        Token {
+            value: Some(value.to_owned()),
+            kind: kind
+        }
+    }
+
     fn is_not_end(&self) -> bool {
-        match self {
-            &Token::InputEnd => false,
+        match self.kind {
+            TokenKind::InputEnd => false,
             _ => true
         }
+    }
+
+    fn value(&self) -> Option<&str> {
+        self.value.as_ref().map(String::as_ref)
     }
 }
 
@@ -87,22 +141,60 @@ impl KnowledgeBase {
 
     fn gen_output<Rand>(&self, rng: &mut Rand) -> String where Rand: Rng{
         let mut out = String::new();
-        let mut last_token = &Token::InputStart;
+        let mut last_token = &Token::new(TokenKind::InputStart);
         while last_token.is_not_end() {
             // Unwrap because the only time this will happen is if the database got corrupted or if
             // we don't have anything inserted yet. TODO add better behavior here later (maybe
             // default contents of the knowledgebase?).
             let context = self.tokens.get(last_token).unwrap();
             last_token = context.get_weighted(rng.gen_range(0, context.total_weight()));
-            if let &Token::Word(ref s) = last_token {
+            if let Some(s) = last_token.value() {
                 out.push_str(s);
                 out.push_str(" ");
             }
-            println!("{}", &out);
         }
         out.pop();
         out
     }
+}
+
+const TOKEN_PATTERNS: [(TokenKind, &'static str); 3] = [
+    (TokenKind::Space, "\\s"),
+    (TokenKind::Punct, ".:;?!,"),
+    (TokenKind::Word, "\\w'")
+];
+
+lazy_static! {
+    static ref TOKEN_REGEX: Regex = {
+        let mut s = String::new();
+        s.push_str("^(");
+        for &(kind, chars) in TOKEN_PATTERNS.iter() {
+            s.push_str(&format!("(?P<{}>[{}]+)|", kind.as_str(), chars));
+        }
+        s.pop();
+        s.push_str(")(?P<rest>.*)$");
+        Regex::new(&s).unwrap()
+    };
+}
+
+fn tokenize(s: &str) -> Vec<Token> {
+    let mut curr = s.clone();
+    let mut tokens: Vec<Token> = vec![Token::new(TokenKind::InputStart)];
+    'capture_loop: while curr.len() > 0 {
+        let caps = TOKEN_REGEX.captures(curr).expect("Unexpected character encountered.");
+        for &(kind, _) in TOKEN_PATTERNS.iter() {
+            if let Some(s) = caps.name(kind.as_str()) {
+                if kind != TokenKind::Space {
+                    tokens.push(Token::with_value(kind, s));
+                }
+                curr = caps.name("rest").expect("Regex error.");
+                continue 'capture_loop;
+            }
+        }
+        panic!("Unexpected character encountered.");
+    }
+    tokens.push(Token::new(TokenKind::InputEnd));
+    tokens
 }
 
 fn main() {
@@ -113,13 +205,12 @@ fn main() {
     print!("User: ");
     stdout.flush().unwrap();
     for line in stdin.lock().lines().map(|l| l.unwrap()) {
-        let token_pairs = line.split_whitespace().map(|word| Token::Word(word.to_owned()));
-        let mut left = Token::InputStart;
-        for right in token_pairs {
+        let mut tokens_iter = tokenize(&line).into_iter();
+        let mut left = tokens_iter.next().unwrap();
+        while let Some(right) = tokens_iter.next() {
             kb.add_pair(&left, &right);
             left = right;
         }
-        kb.add_pair(&left, &Token::InputEnd);
 
         println!(">> {}", kb.gen_output(&mut rng));
 
